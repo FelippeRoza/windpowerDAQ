c2550e13c66aed9a130b674ca7701dcbf45e5ff0
FelippeRoza <felippesroza@gmail.com>
2017-10-19 10:40:33 -0200

    added fast switching pwm to control DC-DC converter

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,8 +1,20 @@
+#include <PWM.h>
+
+int pin_out = 9;
 int d = 1;
+float output = 0.0;
+int input = 10;
 
 void setup() {
+  //initialize all timers except for 0, to save time keeping functions
+  //just because you init the timer doesn't change the default frequency until you call setpinfreq 
+  InitTimersSafe(); 
 
-  // All pins to input
+  //sets the frequency for the specified pin
+  bool success = SetPinFrequencySafe(pin_out, 20000);
+  //success == true if it worked
+
+  // Analog pins as input
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
@@ -13,11 +25,20 @@ void setup() {
   // Init Serial
   Serial.begin(115200);
 
+  output = map(input, 0, 100, 0, 400);
+  analogWrite(pin_out, output);
+
 }// end setup
 
 void send_serial(char type, int analog){
-  Serial.print(type);
-  Serial.println(analog);
+  output = analog*5.0/1023.0;
+  if(type == 'i'){
+    output = (output - 2.5)/0.185;
+  }
+  else if(type == 'v'){
+    output = output*10.0/110.0;
+  }
+  Serial.println(String(type) + " :" +  String(output));
   delayMicroseconds(d);
 }
 
